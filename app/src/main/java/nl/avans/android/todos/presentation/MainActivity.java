5eb4424527a56c5d19162f6dd8aca2e7a596a68a
Robin Schellius <r.schellius@avans.nl>
2017-05-11 22:31:16 +0200

    initial commit

---
+++ app/src/main/java/nl/avans/android/todos/presentation/MainActivity.java
@@ -0,0 +1,155 @@
+package nl.avans.android.todos.presentation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.util.Log;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import nl.avans.android.todos.R;
+import nl.avans.android.todos.domain.ToDo;
+import nl.avans.android.todos.domain.ToDoAdapter;
+import nl.avans.android.todos.service.ToDoApiConnector;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener,
+        ToDoApiConnector.ToDoListener {
+
+    public final String TAG = this.getClass().getSimpleName();
+
+    public final static String TODO_DATA = "PRODUCT";
+
+    private ListView listViewProducts;
+    private final String todoApiUrl = "https://mynodetodolistserver.herokuapp.com/api/v1/todos";
+    private BaseAdapter productAdapter;
+    private ArrayList<ToDo> toDos = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        listViewProducts = (ListView) findViewById(R.id.listViewToDos);
+        listViewProducts.setOnItemClickListener(this);
+        productAdapter = new ToDoAdapter(this, getLayoutInflater(), toDos);
+        listViewProducts.setAdapter(productAdapter);
+
+        // Vul de lijst met ToDos
+        ToDoApiConnector apiConnector = new ToDoApiConnector(this);
+        apiConnector.execute(todoApiUrl);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "Position " + position + " is geselecteerd");
+
+        ToDo toDo = toDos.get(position);
+        Intent intent = new Intent(getApplicationContext(), ToDoDetailActivity.class);
+        intent.putExtra(TODO_DATA, toDo);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onToDosAvailable(ArrayList<ToDo> toDoArrayList) {
+
+        Log.i(TAG, "We hebben " + toDoArrayList.size() + " items in de lijst");
+
+        toDos.clear();
+        for(int i = 0; i < toDoArrayList.size(); i++) {
+            toDos.add(toDoArrayList.get(i));
+        }
+        productAdapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/nl/avans/android/todos/service/ToDoApiConnector.java
@@ -0,0 +1,163 @@
+package nl.avans.android.todos.service;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import nl.avans.android.todos.domain.ToDo;
+
+public class ToDoApiConnector extends AsyncTask<String, Void, String> {
+
+    private ToDoListener listener = null;
+    private String urlString = "";
+    private final String TAG = this.getClass().getSimpleName();
+
+    // We retourneren de hele lijst met producten.
+    private ArrayList<ToDo> toDoArrayList = new ArrayList<>();
+
+    /**
+     *
+     * @param listener
+     */
+    public ToDoApiConnector(ToDoListener listener){
+        this.listener = listener;
+    }
+
+    /**
+     *
+     * @param params
+     * @return
+     */
+    @Override
+    protected String doInBackground(String... params){
+
+        if(params[0] != null) {
+            urlString = params[0];
+        }
+        Log.i(TAG, "doInBackground " + urlString);
+
+        InputStream inputStream = null;
+        int responsCode = -1;
+        String response = "";
+
+        try{
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+
+            if (!(urlConnection instanceof HttpURLConnection)) {
+                // Url
+                return null;
+            }
+
+            HttpURLConnection httpConnection = (HttpURLConnection) urlConnection;
+            httpConnection.setAllowUserInteraction(false);
+            httpConnection.setInstanceFollowRedirects(true);
+            httpConnection.setRequestMethod("GET");
+            httpConnection.connect();
+
+            responsCode = httpConnection.getResponseCode();
+
+            if (responsCode == HttpURLConnection.HTTP_OK) {
+                inputStream = httpConnection.getInputStream();
+                response = getStringFromInputStream(inputStream);
+            }
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "doInBackground MalformedURLEx " + e.getLocalizedMessage());
+            return null;
+        } catch (IOException e) {
+            Log.e("TAG", "doInBackground IOException " + e.getLocalizedMessage());
+            return null;
+        }
+        return response;
+    }
+
+    /**
+     *
+     * @param response
+     */
+    protected void onPostExecute(String response){
+
+        JSONArray jsonArray;
+
+        Log.i(TAG, "onPostExecute");
+
+        // Check of we een response hebben
+        if(response == "") {
+            Log.e(TAG, "onPostExecute - response input was leeg!");
+            return;
+        }
+
+        try{
+            jsonArray = new JSONArray(response);
+
+            for(int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonProduct = jsonArray.getJSONObject(i);
+
+                // Convert stringdate to Date
+                String timestamp = jsonProduct.getString("AangemaaktOp");
+                DateTime todoDateTime = ISODateTimeFormat.dateTimeParser().parseDateTime(timestamp);
+
+                ToDo toDo = new ToDo(
+                        jsonProduct.getString("Titel"),
+                        jsonProduct.getString("Beschrijving"),
+                        jsonProduct.getString("Status"),
+                        todoDateTime
+                );
+                Log.i(TAG, "ToDo: " + toDo);
+
+                toDoArrayList.add(toDo);
+            }
+            // Stuur de resultaten terug
+            listener.onToDosAvailable(toDoArrayList);
+
+        } catch( JSONException ex) {
+            Log.e(TAG, "onPostExecute JSONException " + ex.getLocalizedMessage());
+        }
+    }
+
+    private static String getStringFromInputStream(InputStream inputStream){
+        BufferedReader br = null;
+        StringBuilder sb = new StringBuilder();
+
+        String line;
+        try {
+            br = new BufferedReader(new InputStreamReader(inputStream));
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public interface ToDoListener {
+        void onToDosAvailable(ArrayList<ToDo> toDos);
+    }
+}
