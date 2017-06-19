6d6482b498ca9df6b1bc9fb8c9a0c33591deaad0
Jordy Huijgens <jhuijgens1@live.nl>
2017-06-19 11:22:07 +0200

    fdfdfd

--- api/todo.routes.v1.js
+++ api/todo.routes.v1.js
@@ -77,8 +77,8 @@ routes.post('/rentals/:userid/:inventoryid', function(req, res) {
     console.log(dateNow);
 
     var query = {
-        sql: 'INSERT INTO `rental`(`rental_date`, `inventory_id`, `customer_id`, `staff_id`) VALUES (?,?,?,?);',
-        values: ["'2015-15-14'", 1, 1,1],
+        sql: 'INSERT INTO `rental`(`rental_date`, `inventory_id`, `customer_id`, `staff_id`) VALUES (?,?,?,1);',
+        values: ["'"+dateNow+"'", inventoryid, userid],
         timeout: 2000 // 2secs
     };
     console.log('Onze query: ' + query.sql);
