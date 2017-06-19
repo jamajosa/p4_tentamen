f0f856c2ea5f93675018603c8e1fa1852c706fe9
Jordy Huijgens <jhuijgens1@live.nl>
2017-06-19 15:38:22 +0200

    login en register

--- api/authentication.routes.v1.js
+++ api/authentication.routes.v1.js
@@ -6,40 +6,65 @@ var router = express.Router();
 
 var auth = require('../auth/authentication');
 
-//
-// Hier gaat de gebruiker inloggen.
-// Input: username en wachtwoord
-// ToDo: 
-//	 - zoek de username in de database, en vind het password dat opgeslagen is
-// 	 - als user gevonden en password matcht, dan return valide token
-//   - anders is de inlogpoging gefaald - geef foutmelding terug.
-//
+///////////////////////////////////////////////////////////
+//Login
+///////////////////////////////////////////////////////////
 router.post('/login', function(req, res) {
+    console.dir(req.body);
 
-    // Even kijken wat de inhoud is
-    // console.dir(req.body);
-
-    // De username en pwd worden meegestuurd in de request body
     var username = req.body.username;
     var password = req.body.password;
 
-    // Dit is een dummy-user - die haal je natuurlijk uit de database.
-    // Momenteel zetten we ze als environment variabelen. (Ook op Heroku!)
-    var _dummy_username = process.env.APP_USERNAME || "username";
-    var _dummy_password = process.env.APP_PASSWORD || "test";
+    db.query('SELECT `email`, `password` FROM `customer` WHERE `email` = "'+username+'"', function(error, rows, fields) {
+        if (error) {
+           console.dir(error);
+        } else {
+            console.log(rows);
+            try{
+                var dbUsername = rows[0].email;
+                var dbPassword = rows[0].password
 
-    // Kijk of de gegevens matchen. Zo ja, dan token genereren en terugsturen.
-    if (username == _dummy_username && password == _dummy_password) {
-        var token = auth.encodeToken(username);
-        res.status(200).json({
-            "token": token,
-        });
-    } else {
-        // console.log('Input: username = ' + username + ', password = ' + password);
-        res.status(401).json({ "error": "Invalid credentials, bye" })
-    }
+            if (username == dbUsername && password == dbPassword) {
+                var token = auth.encodeToken(username);
+                res.status(200).json({"token": token,});
+            } else {
+            console.log('Input: username = ' + username + ', password = ' + password);
+            res.status(401).json({ "error": "Invalid credentials, bye" })
+            }
+            } catch(e){
+                res.status(401).json({ "error": "Invalid credentials, bye" })
+            }
+        };
+    });
+
+
 
 });
 
+///////////////////////////////////////////////////////////
+//registreren
+///////////////////////////////////////////////////////////
+router.post('/register', function(request, response){
+console.dir(request.body);
+
+    var customer = request.body;
+    var query = {
+        sql: 'INSERT INTO `customer`(`email`, `password`) VALUES (?, ?)',
+        values: [customer.email, customer.password],
+        timeout: 2000 
+    };
+
+    console.dir(customer);
+    console.log('Onze query: ' + query.sql);
+
+    response.contentType('application/json');
+    db.query(query, function(error, rows, fields) {
+        if (error) {
+            response.status(401).json(error);
+        } else {
+            response.status(200).json({ result: rows });
+        };
+    });
+});
 // Hiermee maken we onze router zichtbaar voor andere bestanden. 
 module.exports = router;
